b8f3bdedfe47147ebb4e4c190f0f931c4954f38b
Ken Irwin <irwinkr@miamioh.edu>
2021-04-28 14:50:18 -0400

    adobe-fetch-test works ok

--- adobe-fetch-test.js
+++ adobe-fetch-test.js
@@ -1,13 +1,11 @@
-const AdobeUserMgmtApi = require('./classes/AdobeUserMgmtApi');
 const adobeConf = require('./config/adobe');
-const AdobeService = require('./classes/AdobeUserMgmtService');
-const adobe = new AdobeUserMgmtApi(adobeConf);
-const serv = new AdobeService(adobeConf);
+const AdobeRepo = require('./repositories/AdobeRepository');
+const adobe = new AdobeRepo(adobeConf);
 const appConf = require('./config/appConf');
 const LicenseGroup = require('./classes/LicenseGroup');
 const lg = new LicenseGroup(appConf);
 
-let adobeLicenses = lg.getVendorGroupNamesByVendor('Adobe');
+// let adobeLicenses = lg.getVendorGroupNamesByVendor('Adobe');
 // console.log(adobeLicenses);
 
 // let group = adobeLicenses[1];
@@ -15,12 +13,8 @@ let group = 'Library API test';
 
 (async () => {
   try {
-    // await adobe.getToken();
-    // console.log(serv);
-    let res = await serv.getGroupMembers(group);
-    // let res = await adobe.getQueryResults(qc);
+    let res = await adobe.getGroupMembers(group);
     console.log(res);
-    // console.log(fetchres);
   } catch (err) {
     console.error('Unable to do the thing:', err);
   }
