d3c9a22de8a53db2d32706e8e22303d7ff2686b8
Ken Irwin <irwinkr@miamioh.edu>
2021-04-29 17:38:42 -0400

    logger in adobeService

--- services/adobeService.js
+++ services/adobeService.js
@@ -7,16 +7,19 @@ const libCal = new LibCalRepo(libCalConf);
 const appConf = require('../config/appConf');
 const LicenseGroup = require('../classes/LicenseGroup');
 const licenses = new LicenseGroup(appConf);
+const logger = require('./logger');
 
 const software = licenses.getLicenseGroupsByVendor('Adobe');
 console.log(software);
+
+logger.info('starting AdobeService');
 
 software.forEach((pkg) => {
   // get libCalList based on pkg.libCalCid
   // get adobe list based on pkg.vendorGroupName
   // convert emails if necessary?
-  // get users to add in Adobe
-  // get users to remove in Adobe
+  // compare: get users to add in Adobe
+  // compare: get users to remove in Adobe
   // adobe remove
   // adobe add
 });
