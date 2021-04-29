df3c132c2643009420255c7114f2e563dceb4a69
Ken Irwin <irwinkr@miamioh.edu>
2021-04-29 15:15:06 -0400

    update package.json (should have done this sooner -- last two commits might not work)

--- libcal-cid-test.js
+++
@@ -1,8 +0,0 @@
-const conf = require('./config/libCal');
-const LibCalApi = require('./models/LibCalApi');
-const api = new LibCalApi(conf);
-
-(async () => {
-  let res = await api.getSoftwareCategories();
-  console.log(JSON.stringify(res));
-})();
