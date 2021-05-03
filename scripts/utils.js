b4d694937bafa91190d6493bf5d2a3041e8e1972
Ken Irwin <irwinkr@miamioh.edu>
2021-05-03 10:36:09 -0400

    activate adobe update from app.js if config says to

--- scripts/utils.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  Divider: function () {
-    console.log('============================================');
-  }
-}
-
--- services/adobeService.js
+++ services/adobeService.js
@@ -12,56 +12,61 @@ const Utils = require('../classes/Utils');
 const utils = new Utils();
 let software = licenses.getLicenseGroupsByVendor('Adobe');
 software = software.filter((i) => parseInt(i.libCalCid) > 20000);
-console.log(software);
 
-logger.info('starting AdobeService');
+module.exports = async () => {
+  logger.info('starting AdobeService');
 
-utils.asyncForEach(software, async (pkg) => {
-  logger.info(
-    `Getting libCalCid: ${pkg.libCalCid}, vendorGroupName: ${pkg.vendorGroupName}`
-  );
+  utils.asyncForEach(software, async (pkg) => {
+    logger.info(
+      `Getting libCalCid: ${pkg.libCalCid}, vendorGroupName: ${pkg.vendorGroupName}`
+    );
 
-  // get libCalList based on pkg.libCalCid
-  let libCalBookings = await libCal.getCurrentValidBookings(pkg.libCalCid);
-  // console.log(pkg.libCalCid, libCalBookings.length);
-  let libCalEmails = libCal.getUniqueEmailsFromBookings(libCalBookings);
-  logger.debug('libCalEmails:', { content: libCalEmails });
-  // // get adobe list based on pkg.vendorGroupName
-  let currAdobeEntitlements = await adobe.getGroupMembers(pkg.vendorGroupName);
-  // console.log('currAdobeEntitlements:', currAdobeEntitlements.length);
-  let currAdobeEmails = adobe.getEmailsFromGroupMembers(currAdobeEntitlements);
-  logger.debug('currAdobeEmails:', { content: currAdobeEmails });
+    // get libCalList based on pkg.libCalCid
+    let libCalBookings = await libCal.getCurrentValidBookings(pkg.libCalCid);
+    // console.log(pkg.libCalCid, libCalBookings.length);
+    let libCalEmails = libCal.getUniqueEmailsFromBookings(libCalBookings);
+    logger.debug('libCalEmails:', { content: libCalEmails });
+    // // get adobe list based on pkg.vendorGroupName
+    let currAdobeEntitlements = await adobe.getGroupMembers(
+      pkg.vendorGroupName
+    );
+    // console.log('currAdobeEntitlements:', currAdobeEntitlements.length);
+    let currAdobeEmails = adobe.getEmailsFromGroupMembers(
+      currAdobeEntitlements
+    );
+    logger.debug('currAdobeEmails:', { content: currAdobeEmails });
 
-  // Fake Data: to use this, comment out the code above and uncomment these two lines
-  // let libCalBookings = ['irwinkr@miamioh.edu', 'bomholmm@miamioh.edu'];
-  // let currAdobeEmails = ['irwinkr@miamioh.edu', 'qum@miamioh.edu'];
+    // Fake Data: to use this, comment out the code above and uncomment these two lines
+    // let libCalBookings = ['irwinkr@miamioh.edu', 'bomholmm@miamioh.edu'];
+    // let currAdobeEmails = ['irwinkr@miamioh.edu', 'qum@miamioh.edu'];
 
-  // we're skipping this step so far:
-  // convert emails if necessary?
+    // we're skipping this step so far:
+    // convert emails if necessary?
 
-  // compare: get users to remove in Adobe
-  let emailsToRemove = utils.filterToEntriesMissingFromSecondArray(
-    currAdobeEmails,
-    libCalEmails
-  );
+    // compare: get users to remove in Adobe
+    let emailsToRemove = utils.filterToEntriesMissingFromSecondArray(
+      currAdobeEmails,
+      libCalEmails
+    );
 
-  // compare: get users to add in Adobe
-  let emailsToAdd = utils.filterToEntriesMissingFromSecondArray(
-    libCalEmails,
-    currAdobeEmails
-  );
+    // compare: get users to add in Adobe
+    let emailsToAdd = utils.filterToEntriesMissingFromSecondArray(
+      libCalEmails,
+      currAdobeEmails
+    );
 
-  // adobe remove
-  logger.info('Adobe Remove:', { content: emailsToRemove });
-  if (emailsToRemove.length > 0) {
-    res = await adobe.removeGroupMembers(emailsToRemove, pkg.vendorGroupName);
-    logger.info(res);
-  }
+    // adobe remove
+    logger.info('Adobe Remove:', { content: emailsToRemove });
+    if (emailsToRemove.length > 0) {
+      res = await adobe.removeGroupMembers(emailsToRemove, pkg.vendorGroupName);
+      logger.info(res);
+    }
 
-  // adobe add
-  logger.info('Adobe Add:', { content: emailsToAdd });
-  if (emailsToAdd.length > 0) {
-    res = await adobe.addGroupMembers(emailsToAdd, pkg.vendorGroupName);
-    logger.info(res);
-  }
-});
+    // adobe add
+    logger.info('Adobe Add:', { content: emailsToAdd });
+    if (emailsToAdd.length > 0) {
+      res = await adobe.addGroupMembers(emailsToAdd, pkg.vendorGroupName);
+      logger.info(res);
+    }
+  });
+};
