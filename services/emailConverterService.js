c4da5db92eed483bacb62f669801bc2da17c9fa3
Ken Irwin <irwinkr@miamioh.edu>
2021-05-04 10:54:01 -0400

    outline  emailConverterService

---
+++ services/emailConverterService.js
@@ -0,0 +1,7 @@
+/*
+- take in an array of emails
+    - request authoritative emails from db
+    - request any stragglers from emailConverter API
+- return array of authoritative emails
+- log any remaining stragglers to error log
+*/
