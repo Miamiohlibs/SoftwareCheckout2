cd69d0d04bb36cb00acf990fd55e057d819fa768
Ken Irwin <irwinkr@miamioh.edu>
2021-05-10 11:14:34 -0400

    removed commented-out code from logger

--- services/logger.js
+++ services/logger.js
@@ -3,14 +3,11 @@ const path = require('path');
 const dayjs = require('dayjs');
 const today = dayjs().format('YYYY-MM-DD');
 const month = dayjs().format('YYYY-MM');
-// const infolog = path.join(__dirname, '..', 'logs', 'info-' + month + '.log');
-// const debuglog = path.join(__dirname, '..', 'logs', 'debug-' + today + '.log');
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, prettyPrint } = format;
 
 transportsArr = [];
 for (level in appConf.logLevels) {
-  // console.log(level, appConf.logLevels[level]);
   filename = '';
   if (appConf.logLevels[level] == 'daily') {
     filename = path.join(__dirname, '..', 'logs', level + '-' + today + '.log');
