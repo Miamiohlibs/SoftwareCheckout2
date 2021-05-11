6f245c4021ba0d71b5ff2a11d857bfd055e1ae5b
Ken Irwin <irwinkr@miamioh.edu>
2021-05-11 13:51:12 -0400

    emailConverterService exports effective solution

--- services/emailConverterService.js
+++ services/emailConverterService.js
@@ -1,3 +1,10 @@
+const appConf = require('../config/appConf');
+const UniqEmailRepo = require('../repositories/UniqEmailRepository');
+const emailRepo = new UniqEmailRepo();
+const emailConverterRepo = require('../repositories/EmailConverterRepository');
+const logger = require('./logger');
+const convertRepo = new emailConverterRepo(appConf);
+
 /*
 - take in an array of emails
     - request authoritative emails from db
@@ -5,3 +12,20 @@
 - return array of authoritative emails
 - log any remaining stragglers to error log
 */
+
+module.exports = async (emails) => {
+  await emailRepo.connect();
+  knownEmails = await emailRepo.querySpecificEmails(emails);
+  let { found, missing } = await emailRepo.getKnownAndUnknownEmails(
+    emails,
+    knownEmails
+  );
+  let authoritativeEmails = found;
+  let {
+    authFound,
+    authMissing,
+  } = await convertRepo.getAuthoritativeEmailsBatch(missing);
+  await emailRepo.disconnect();
+  logger.error('Failed to get authoritative emails for:', authMissing);
+  return authoritativeEmails.concat(authFound);
+};
