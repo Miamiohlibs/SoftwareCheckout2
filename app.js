965633fa2392381b716bf5057948a78cdd38203a
Ken Irwin <irwinkr@miamioh.edu>
2021-05-21 17:05:26 -0400

    app.js should be wrapped in a self-starting async

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ const lg = new LicenseGroup(appConf);
 const adobe = require('./services/AdobeService');
 const jamf = require('./services/jamfService');
 let vendors = lg.getActiveVendors();
+let logger = require('./services/logger');
 
-async () => {
+logger.info('starting app');
+
+(async () => {
   if (vendors.includes('Adobe')) {
+    logger.info('starting Adobe service from app.js');
     await adobe();
   }
 
   if (vendors.includes('Jamf')) {
+    logger.info('starting jamf service from app.js');
     await jamf();
   }
-};
+})();
