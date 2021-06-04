ad4a5840065a198d234446e8643953cf463624a8
Ken Irwin <irwinkr@miamioh.edu>
2021-06-04 16:58:04 -0400

    add config/jamf.sample

---
+++ config/jamf.sample.js
@@ -0,0 +1,10 @@
+module.exports = {
+  auth: {
+    username: 'jamf-user',
+    password: 'jamf-user-pass',
+  },
+  baseUrl: 'https://yourjamfinstance.fake.org:8443',
+  emailSuffix: '@fake.org',
+  userGroupName: 'Name of your user Jamf User Management Group',
+  danger_tls_reject_unauthorized: false,
+};
