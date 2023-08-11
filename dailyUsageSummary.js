177055638849ab10f1021bd466dce493c08a8422
Ken Irwin <ken@kenirwin.net>
2023-08-11 14:59:08 -0400

    fix date sort in daily usage summary

--- dailyUsageSummary.js
+++ dailyUsageSummary.js
@@ -19,7 +19,7 @@ function summarizeStats(softwareTitles) {
     files.push(...anonFiles); // all files, anonymous and not
     files = [...new Set(files)]; // remove duplicates
     files = files.filter((file) => file.isFile()); // remove directories
-    files.sort(); // sort by date
+    files.sort((a, b) => (a.name > b.name ? 1 : -1)); // sort by date
     files.forEach((file) => {
       let date = file.name.split('.')[0];
       getDateStats(softwareTitle, date);
