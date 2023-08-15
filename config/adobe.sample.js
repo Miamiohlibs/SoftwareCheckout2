5d1ef80b34669c3279ee565738d1e4ae5ad01b21
Ken Irwin <ken@kenirwin.net>
2023-08-15 11:04:16 -0400

    remove old adobe queryConf from config/adobe.sample

--- config/adobe.sample.js
+++ config/adobe.sample.js
@@ -11,39 +11,6 @@ module.exports = {
     privateKeyFile: '../certs/private.key', // You will generate this key and upload the public key to Adobe
   },
 
-  /* Do not edit below this line */
-
-  queryConf: {
-    getGroups: {
-      options: {
-        hostname: 'usermanagement.adobe.io',
-        port: 443,
-        pathStem: '/v2/usermanagement/groups/',
-        // path: configure at query time
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-          // Authorization: configure at query time
-          // x-api-key: configure at query time
-        },
-      },
-    },
-    generic: {
-      options: {
-        hostname: 'usermanagement.adobe.io',
-        port: 443,
-        pathStem: '/v2/usermanagement/',
-        // path: configure at query time
-        // method: configure at query time
-        headers: {
-          Accept: 'application/json',
-          // Authorization: configure at query time
-          // x-api-key: configure at query time
-        },
-      },
-    },
-  },
-
   // savings calculator configuration, estimate costs saved by using Adobe Checkout
   // default settings assume a $20/month student license and a 14-day checkout period
   // your mileage may vary
