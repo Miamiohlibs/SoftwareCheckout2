b9ccec3aa5d3dfe8b9221dfa0dceb8d6174cd086
Ken Irwin <ken@kenirwin.net>
2023-12-07 16:29:23 -0500

    prettier format for usageSummary output

--- usageSummary.sh
+++ usageSummary.sh
@@ -9,15 +9,18 @@ START_YEAR=2020
 END_YEAR="$(date +%Y)"
 
 for directory in ./logs/dailyStats/*; do
-    echo "Directory: $directory"
+    echo ""
+    echo "------------------------------------"
+    sed -r "s/^.*\/(.*)$/\1/" <<< $directory
+    echo "------------------------------------"
     for ((i=$START_YEAR; i<=$END_YEAR; i++)) 
     do
         if compgen -G "$directory/anon/$i*" > /dev/null ; then
         echo "Year: $i"
         bookings=`jq '.[] | select(.status == "Confirmed") | {bookId} ' $directory/anon/$i* | grep bookId | sort | uniq | wc -l`
         users=`jq '.[] | select(.status == "Confirmed") | {email} ' $directory/anon/$i* | grep email | sort | uniq | wc -l`
-        echo "  Bookings:$bookings"
-        echo "  Users:$users"
+        echo "  Bookings: $bookings"
+        echo "  Users: $users"
         fi
     done
 done
