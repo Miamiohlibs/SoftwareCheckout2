a401f60ea5889b8ac33ae8d152fad3395d365c28
Ken Irwin <ken@kenirwin.net>
2023-12-07 16:25:10 -0500

    added comments to usageSummary.sh

--- usageSummary.sh
+++ usageSummary.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# for each directory in ./logs/dailyStats
+#   for each year in the directory
+#       if there are files for that year
+#           count the number of bookings and distinct users from anonymised files
+
 START_YEAR=2020
 END_YEAR="$(date +%Y)"
 
@@ -7,11 +12,6 @@ for directory in ./logs/dailyStats/*; do
     echo "Directory: $directory"
     for ((i=$START_YEAR; i<=$END_YEAR; i++)) 
     do
-        # if [ ! -f "$directory/anon/$i"* ] ; then
-        # if 
-        #     continue
-        # fi
-
         if compgen -G "$directory/anon/$i*" > /dev/null ; then
         echo "Year: $i"
         bookings=`jq '.[] | select(.status == "Confirmed") | {bookId} ' $directory/anon/$i* | grep bookId | sort | uniq | wc -l`
@@ -19,12 +19,5 @@ for directory in ./logs/dailyStats/*; do
         echo "  Bookings:$bookings"
         echo "  Users:$users"
         fi
-
-
-        # echo "Year: $i"
-        # bookings=`jq '.[] | select(.status == "Confirmed") | {bookId} ' $directory/anon/$i* | grep bookId | sort | uniq | wc -l`
-        # users=`jq '.[] | select(.status == "Confirmed") | {email} ' $directory/anon/$i* | grep email | sort | uniq | wc -l`
-        # echo "  Bookings:$bookings"
-        # echo "  Users:$users"
     done
 done
