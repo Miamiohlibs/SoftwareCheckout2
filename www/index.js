8d1a5e90a7dbb8831c1af1b2063ab78ae5c6f54d
Ken Irwin <ken@kenirwin.net>
2024-09-04 11:18:02 -0400

    the beginnings of an express-based console

--- www/index.js
+++ www/index.js
@@ -1,11 +1,21 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3010;
 
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 let apiRouter = require('./routes/api');
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', async (req, res) => {
+  //   res.send('Hello World!');
+  let data = await fetch(`http://localhost:${port}/api/groups`);
+  let json = await data.json();
+  //   res.json(json);
+  res.render('main', { data: json });
 });
+
+app.use('/api', apiRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- www/routes/api.js
+++ www/routes/api.js
@@ -1,4 +1,103 @@
+const express = require('express');
+const router = express.Router();
+const { text } = require('express');
+const appConf = require('../../config/appConf');
+const LicenseGroup = require('../../helpers/LicenseGroup');
+const lg = new LicenseGroup(appConf);
+const {
+  filterToEntriesMissingFromSecondArray,
+} = require('../../helpers/utils');
+const baseUrl = 'http://localhost:3010';
+
+async function getAdobeBookingsByGroup(group) {
+  const adobeConf = require('../../config/adobe');
+  const AdobeRepository = require('../../repositories/AdobeRepository');
+  const adobe = new AdobeRepository(adobeConf);
+  let bookings = await adobe.getGroupMembers(group);
+  return bookings;
+}
+
+async function getLibCalBookingsByCid(cid) {
+  const libcalConf = require('../../config/libcal');
+  const LibCalRepository = require('../../repositories/LibCalRepository');
+  const libcal = new LibCalRepository(libcalConf);
+  let bookings = await libcal.getCurrentValidBookings(cid);
+  return bookings;
+}
+
 router.get('/vendors', async (req, res) => {
-  let vendors = await vendor.getAllVendors();
-  res.send(vendors);
+  //   res.json({ text: 'Hello World!' });
+  let vendors = await lg.getActiveVendors();
+  res.json(vendors);
 });
+
+router.get('/libcal', async (req, res) => {
+  const libcalConf = require('../../config/libcal');
+  const LibCalRepository = require('../../repositories/LibCalRepository');
+  const libcal = new LibCalRepository(libcalConf);
+  let group = req.query.group;
+  let bookings = await libcal.getCurrentValidBookings(group);
+  if (req.query.hasOwnProperty('emailOnly')) {
+    let emailOnly = req.query.emailOnly;
+    if (emailOnly === 'true') {
+      let emails = libcal.getUniqueEmailsFromBookings(bookings);
+      res.json(emails);
+      return;
+    }
+  }
+  res.json(bookings);
+});
+
+router.get('/groups', async (req, res) => {
+  let vendor = req.query.vendor;
+  let vendors = lg.getActiveVendors();
+  let returnObj = [];
+  for (let i = 0; i < vendors.length; i++) {
+    let vendor = vendors[i];
+    let groups = lg.getLicenseGroupsByVendor(vendor);
+    returnObj.push({ vendor: vendor, groups: groups });
+  }
+  //   let groups = await lg.getVendorGroups(vendor);
+  res.json(returnObj);
+});
+
+router.get('/adobe', async (req, res) => {
+  const bookings = await getAdobeBookingsByGroup(req.query.group);
+  res.json(bookings);
+});
+
+router.get('/adobe/compare', async (req, res) => {
+  let group = req.query.group;
+  let cid = req.query.cid;
+  const adobeBookings = await getAdobeBookingsByGroup(group);
+  const adobeEmails = adobeBookings.map((i) => i.email);
+  const libCalBookings = await getLibCalBookingsByCid(cid);
+  const libCalEmails = libCalBookings.map((i) => i.email);
+  let emailsToRemove = filterToEntriesMissingFromSecondArray(
+    adobeEmails,
+    libCalEmails
+  );
+  let emailsToAdd = filterToEntriesMissingFromSecondArray(
+    libCalEmails,
+    adobeEmails
+  );
+  res.json({
+    emailsToRemove: emailsToRemove,
+    emailsToAdd: emailsToAdd,
+    adobeEmails: adobeEmails,
+    libCalEmails: libCalEmails,
+  });
+
+  // get unique emails from libcalList
+});
+
+router.get('/jamf', async (req, res) => {
+  const jamfConf = require('../../config/jamf');
+  const JamfRepository = require('../../repositories/JamfRepository');
+  const jamf = new JamfRepository(jamfConf);
+  let group = req.query.group;
+  let members = await jamf.getGroupMembers(group);
+  res.json(members);
+});
+
+module.exports = router;
