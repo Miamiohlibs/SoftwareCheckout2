60c0ed51f7990069ea46bb0d3482ec8eb5aeedda
Ken Irwin <ken@kenirwin.net>
2024-10-02 15:53:13 -0400

    spinner-wrapper contents removed from div on pageshow -- fixes spinner bug

--- www/public/js/spinner.js
+++ www/public/js/spinner.js
@@ -1,41 +1,13 @@
-// window.addEventListener('load', function () {
-//   console.log('page loaded');
-//   const spinnerWrapper = document.getElementById('spinner-wrapper');
-//   spinnerWrapper.innerHTML = '';
-//   console.log(spinnerWrapper.innerHTML);
-//   // empty the aside element
-// });
-
-window.onpopstate = () => setTimeout(console.log('mozilla zero timeout'), 0);
-window.onbeforeunload = (event) => {
-  event.preventDefault();
-
-  console.log('page is about to be unloaded');
-  console.log('subsequent log');
-  try {
-    console.log('starting to try to remove spinner');
-    let spinnerWrapper = document.getElementById('spinner-wrapper');
-    spinnerWrapper.innerHTML = '';
-    console.log('finished removing spinner');
-  } catch (err) {
-    console.log('error removing spinner');
-    console.log(err);
-  }
-};
-$(document).on('popstate', function () {
-  //   $('#spinner-wrapper').html('');
-  console.log('page loaded', Date.now());
+window.addEventListener('pageshow', function () {
+  // this code will run when the page is loaded, including from the back/forward button
+  const spinnerWrapper = document.getElementById('spinner-wrapper');
+  spinnerWrapper.innerHTML = '';
+  console.log(spinnerWrapper.innerHTML);
 });
 
 $(document).ready(function () {
-  $('#spinner-wrapper').html('');
-  console.log('page loaded', Date.now());
-  //   alert($('#spinner-wrapper').html());
-  const spinnerWrapper = $('#spinner-wrapper');
-  spinnerWrapper.html('');
-
   $('a').click(function () {
-    if ($(this).attr('href') == '#') {
+    if ($(this).attr('href').startsWith('#')) {
       return;
     }
     setTimeout(() => {
