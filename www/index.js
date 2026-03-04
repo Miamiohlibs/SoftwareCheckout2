1757833771eb876c475a7f523da9ddadf00cb824
Ken Irwin <ken@kenirwin.net>
2026-03-04 10:45:00 -0500

    try webpath in api router declaration

--- www/index.js
+++ www/index.js
@@ -95,7 +95,7 @@ app.use(express.static(__dirname + '/public'));
 
 // Routes
 let apiRouter = require('./routes/api');
-app.use('/api', apiKeyAuth, apiRouter);
+app.use(`${app.locals.webPath}/api`, apiKeyAuth, apiRouter);
 let logsRouter = require('./routes/logs');
 app.use('/logs', isLoggedIn, logsRouter);
 let statsRouter = require('./routes/stats');
