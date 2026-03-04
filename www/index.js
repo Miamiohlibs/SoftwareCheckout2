7d8ccaa0cb002f73a96b698eb0c1886983f666d8
Ken Irwin <ken@kenirwin.net>
2026-03-04 14:43:38 -0500

    add routers

--- www/index.js
+++ www/index.js
@@ -98,13 +98,13 @@ app.use(express.static(__dirname + '/public'));
 // });
 
 // // Routes
-// let apiRouter = require('./routes/api');
-// app.use(`/api`, apiKeyAuth, apiRouter);
-// let logsRouter = require('./routes/logs');
-// app.use('/logs', isLoggedIn, logsRouter);
-// let statsRouter = require('./routes/stats');
-// const { error } = require('console');
-// app.use('/stats', isLoggedIn, statsRouter);
+let apiRouter = require('./routes/api');
+app.use(`/api`, apiKeyAuth, apiRouter);
+let logsRouter = require('./routes/logs');
+app.use('/logs', isLoggedIn, logsRouter);
+let statsRouter = require('./routes/stats');
+const { error } = require('console');
+app.use('/stats', isLoggedIn, statsRouter);
 
 // app.set('json spaces', 2);
 
